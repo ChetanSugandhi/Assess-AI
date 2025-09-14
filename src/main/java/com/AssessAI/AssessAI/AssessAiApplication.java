3d3d3b07c84d317b7aaffcf1004de1cd802ef5dd
Chetan Sugandhi <chetansugandhi6@gmail.com>
2025-09-14 20:25:46 +0530

    Initial commit

---
+++ src/main/java/com/AssessAI/AssessAI/AssessAiApplication.java
@@ -0,0 +1,13 @@
+package com.AssessAI.AssessAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AssessAI/AssessAI/AssessAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AssessAI.AssessAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
