07f0cd96ff67c914a1d2e90fc2dfe64bdf793405
Chetan Sugandhi <chetansugandhi6@gmail.com>
2025-09-18 12:10:25 +0530

    Update

--- src/main/java/com/AssessAI/AssessAI/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AssessAI.AssessAI.repository;
-
-import com.AssessAI.AssessAI.models.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
