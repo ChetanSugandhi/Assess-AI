4c7ca1969d8c75df089e52494d0db06ed5917629
Chetan Sugandhi <chetansugandhi6@gmail.com>
2025-11-14 20:56:50 +0530

    Add swagger

--- src/main/java/com/AssessAI/AssessAI/security/SecurityConfig.java
+++ src/main/java/com/AssessAI/AssessAI/security/SecurityConfig.java
@@ -132,7 +132,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // public endpoints
                         .requestMatchers("/api/users/signup", "/api/users/signin").permitAll() // public endpoints
-                        // everything else requires authentication
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
 
